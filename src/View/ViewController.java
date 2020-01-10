bcd495c6d05e95cbe06cd7e9ff9fa9756d984dd1
TomerGodelli <48795479+TomerGodelli@users.noreply.github.com>
2020-01-10 14:42:16 +0200

    Update ViewController.java

--- src/View/ViewController.java
+++ src/View/ViewController.java
@@ -3,6 +3,7 @@
  */
 package View;
 
+import Model.Model;
 import ViewModel.ViewModel;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
@@ -289,9 +290,9 @@ public class ViewController implements Observer {
 
     public void is_API_synonym(ActionEvent actionEvent) {
         if (click_stream.isSelected()) {
-            myViewModel.click_stream_seaech(true);
+            myViewModel.API_synonym(true);
         } else {
-            myViewModel.click_stream_seaech(false);
+            myViewModel.API_synonym(false);
         }
 
     }
@@ -345,8 +346,12 @@ public class ViewController implements Observer {
             StringBuilder query = myViewModel.queryToString();
             if (query != null) {
                 try {
-                    String pathToSave = (myViewModel.getSemanticTreatment()) ? path + "\\query_result_semantic.txt" : path + "\\query_result.txt";
-                    BufferedWriter bwr = new BufferedWriter(new FileWriter(new File(pathToSave)));
+
+                    String pathToSave = (myViewModel.getSemanticTreatment()) ? path + "\\qr_semantic" : path + "\\qr";
+                    String p2 = (myViewModel.getIsStem()) ? pathToSave+"_stem" : pathToSave;
+                    String p4 = (myViewModel.getIsApiSyn()) ? p2+"_APIsyn" : p2;
+                    String p3 = p4+".txt";
+                    BufferedWriter bwr = new BufferedWriter(new FileWriter(new File(p3)));
 
                     //write contents of StringBuffer to a file
                     bwr.write(query.toString());
@@ -472,8 +477,11 @@ public class ViewController implements Observer {
             labelHb.getChildren().add(label2);
 
             Button show = new Button("show Entity for Document");
+            String style1 = " -fx-text-fill: black; -fx-background-radius: 200px, 200px, 200px, 200px;  -fx-background-color: linear-gradient(#c3ffea, #46ffdd); -fx-effect: dropshadow( three-pass-box , rgba(0,0,0,0.6) , 5, 0.0 , 0 , 1 )";
+            show.setStyle(style1);
             if (noCorpusPath) {
-                show.setDisable(true);
+                show.setVisible(false);
+                label2.setVisible(false);
             }
             show.setOnAction((ActionEvent e) -> {
 
@@ -530,6 +538,8 @@ public class ViewController implements Observer {
             vbox.setPadding(new Insets(10, 10, 0, 10));
             //vbox.setPadding(new Insets(25, 25, 25, 25));
             vbox.getChildren().addAll(labelHb, myTable, select);
+            String style = "-fx-background-color: rgba(72,174,176,0.87);";
+            vbox.setStyle(style);
             // StackPane sp = new StackPane(buttonHb,myTable,label);
             Scene scene = new Scene(vbox, 500, 800);
             Stage stage = new Stage();
