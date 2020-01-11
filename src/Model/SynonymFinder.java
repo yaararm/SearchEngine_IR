6c61e74901239ca7b9bd7df29b31d7fa7e2062ef
yaararm <yaararm@gmail.com>
2020-01-11 18:35:53 +0200

    save

--- src/Model/SynonymFinder.java
+++ src/Model/SynonymFinder.java
@@ -6,6 +6,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
@@ -34,10 +35,7 @@ public class SynonymFinder {
                 if (p != null){
                     scoredWords.add(p);
                 }
-
-
             }
-
         }catch(Exception e){
 
         }
@@ -49,52 +47,30 @@ public class SynonymFinder {
 
     private  Pair<String, Double> getIdentical(String s) throws Exception {
 
-        final String USER_AGENT = "Mozilla/5.0";
+        URL yahoo = new URL("https://api.datamuse.com/words?ml="+s);
+        URLConnection yc = yahoo.openConnection();
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(
+                        yc.getInputStream()));
+        String inputLine;
 
-        HttpURLConnectionExample http = new HttpURLConnectionExample();
+        while ((inputLine = in.readLine()) != null)
+           // System.out.println(inputLine);
 
-        String wordToSearch = s;
-        //http.searchSynonym(wordToSearch);
 
-        //System.out.println("Sending request...");
-        String url = "https://api.datamuse.com/words?ml=" + wordToSearch;
-       // String url = "https://api.datamuse.com/words?rel_syn=" + wordToSearch;
-        //String url = "https://api.datamuse.com/words?rd=" + wordToSearch;
-        URL obj = new URL(url);
-        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-
-        con.setRequestMethod("GET");
-        con.setRequestProperty("User-Agent", USER_AGENT);
-
-        int responseCode = con.getResponseCode();
-        //System.out.println("\nSending request to: " + url);
-        //System.out.println("JSON Response: " + responseCode + "\n");
-
-        // ordering the response
-        StringBuilder response;
-        try (BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()))) {
-            String inputLine;
-            response = new StringBuilder();
-
-            while ((inputLine = in.readLine()) != null) {
-                response.append(inputLine + "\n");
-            }
-        }
-        if (response.toString().isEmpty() ||  response.toString().contains("word")) {
-            int w = response.indexOf("word");
-            int sc = response.indexOf("score");
-            int end = response.indexOf(",",sc);
-            String term = response.substring(w + 7, sc - 3);
-            String score = response.substring(sc + 7, end);
-            //System.out.println(s);
-            //System.out.println("t:"+term+" s:"+score + "nirmul:" +(parseDouble(score)/120000));
-            //System.out.println(response.toString());
+        if (inputLine.toString().isEmpty() ||  inputLine.toString().contains("word")) {
+            int w = inputLine.indexOf("word");
+            int sc = inputLine.indexOf("score");
+            int end = inputLine.indexOf(",", sc);
+            String term = inputLine.substring(w + 7, sc - 3);
+            String score = inputLine.substring(sc + 7, end);
+           // System.out.println(s);
+            //System.out.println("t:" + term + " s:" + score + "nirmul:" + (parseDouble(score) / 120000));
+            //System.out.println(inputLine.toString());
             return new Pair<String, Double>(term, parseDouble(score));
         }
-        else{
-            return null;
-        }
-
+        in.close();
+        return null;
     }
 
     private Set<String> checkScore ( ArrayList<Pair<String,Double>> scoredWords){
